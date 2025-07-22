064a6b44844c4473333612bb5c38cdb895ec86d8
anna <annachen368@gmail.com>
2025-07-21 21:42:49 -0700

    Add Hilt, Retrofit, Room, Moshi, and Coil dependencies for dependency injection, networking, database, JSON parsing, and image loading. Update Gradle plugins and versions.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.ksp) // Required for annotation processing
 }
 
 android {
@@ -40,20 +42,38 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.hilt.android) // Hilt dependency
+    ksp(libs.hilt.compiler) // Hilt compiler
+    implementation(libs.hilt.navigation.compose) // If using Jetpack Compose
+    implementation(libs.okhttp.logging) // Optional: Logging Interceptor
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.gson) // JSON converter
+    implementation(libs.retrofit.moshi) // JSON converter
+    implementation(libs.moshi.kotlin)
+    ksp(libs.moshi.codegen)  // Using KSP for annotation processing
+    implementation(libs.lifecycle.viewmodel)
+    implementation(libs.lifecycle.livedata) // Optional for LiveData
+    implementation(libs.lifecycle.runtime) // Optional for Coroutine support
+    implementation(libs.room.runtime)
+    ksp(libs.room.compiler)
+    implementation(libs.room.ktx)
+    implementation(libs.coil.compose) // Coroutine Image Loader
+    implementation(libs.coil.network) // Coroutine Image Loader
+
     testImplementation(libs.junit)
+    testImplementation(libs.room.testing) // Optional - Testing
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
+
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 }
